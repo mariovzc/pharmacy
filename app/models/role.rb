eebdf9a8f642e7e6976d3974d95a4a1144fe162a
Mario Vizcaino <mario.vizcainos@gmail.com>
2018-08-12 22:40:52 -0500

    add asociations with user

--- app/models/role.rb
+++ app/models/role.rb
@@ -6,8 +6,13 @@
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  status     :boolean          default(TRUE)
 #
 
 class Role < ApplicationRecord
+  #associations
+  has_many :users
+
+  #validations
   validates :name, presence: true
 end
