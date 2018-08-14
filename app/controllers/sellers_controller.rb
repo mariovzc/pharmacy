62fc8efdd1671dacb5c98b1630591b9207adcc56
Mario Vizcaino <mario.vizcainos@gmail.com>
2018-08-13 21:37:58 -0500

    created Seller controllers and views

---
+++ app/controllers/sellers_controller.rb
@@ -0,0 +1,39 @@
+class SellersController < ApplicationController
+  before_action :set_seller, only [:show, :edit, :update, :deactivate]
+
+  def index
+  end
+
+  def new
+    @seller = Seller.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def deactivate
+  end
+
+  private
+
+  def set_seller
+    @seller = Seller.find(params[:id])
+  end
+
+  def seller_params
+    params.require(:seller).permit(
+      :email,
+      :password,
+      :password_confirmation,
+      :first_name,
+      :last_name,
+      :document
+    )
+  end
+end
