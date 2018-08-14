b7ddf550bb8ccbf10ce85c0e8fdc0bf014777cff
Mario Vizcaino <mario.vizcainos@gmail.com>
2018-08-13 21:31:12 -0500

    create models for each user type

---
+++ app/models/seller.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :integer          not null, primary key
+#  email            :string           not null
+#  crypted_password :string
+#  salt             :string
+#  role_id          :integer
+#  first_name       :string
+#  last_name        :string
+#  document         :string
+#  status           :boolean          default(TRUE)
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  role_name        :string
+#
+
+class Seller < User
+end
