86537e0fb526f966cb81cf6ae87319a8781014dd
Mario Vizcaino <mario.vizcainos@gmail.com>
2018-08-14 21:31:25 -0500

    created seller root_path route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get '/admin', to: 'admin_dashboard#index'
+
+  get '/seller', to: 'seller_dashboard#index'
+
   resources :sessions, only: [:new, :create, :destroy]
   get '/log_in', to: 'sessions#new', as: :log_in
   delete '/log_out', to: 'sessions#destroy', as: :log_out
