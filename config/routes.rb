816e6ef0846e43e766877044808b5bcbf1c633d5
Mario Vizcaino <mario.vizcainos@gmail.com>
2018-08-14 22:28:28 -0500

    upload for hound CR

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,15 @@
 Rails.application.routes.draw do
   get '/admin', to: 'admin_dashboard#index'
-
   get '/manager', to: 'manager_dashboard#index'
-
   get '/seller', to: 'seller_dashboard#index'
 
   resources :sessions, only: [:new, :create, :destroy]
   get '/log_in', to: 'sessions#new', as: :log_in
   delete '/log_out', to: 'sessions#destroy', as: :log_out
-    
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :sellers, except: [:destroy] do 
+
+  resources :sellers, except: [:destroy] do
     member do
       put :deactivate
     end
   end
-
 end
--- spec/controllers/sellers_controller_spec.rb
+++ spec/controllers/sellers_controller_spec.rb
@@ -3,5 +3,4 @@
 require 'rails_helper'
 
 RSpec.describe SellersController, type: :controller do
-
 end
