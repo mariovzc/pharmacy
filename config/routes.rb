f9e9fa092a3708296a2e4a8cd245ff7a1b431769
Mario Vizcaino <mario.vizcainos@gmail.com>
2018-08-16 22:02:17 -0500

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/manager', to: 'manager_dashboard#index'
   get '/seller', to: 'seller_dashboard#index'
 
+  root 'sessions#new'
   resources :sessions, only: [:new, :create, :destroy]
   get '/log_in', to: 'sessions#new', as: :log_in
   delete '/log_out', to: 'sessions#destroy', as: :log_out
