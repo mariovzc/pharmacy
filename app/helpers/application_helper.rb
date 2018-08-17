bcd7521ef6c583e160fb2acadb3bdd5c94ca0704
Mario Vizcaino <mario.vizcainos@gmail.com>
2018-08-16 22:53:46 -0500

    created helper for status and price

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  #this validate the status of a element and return it in spanish!
+  def status_helper(status)
+    status ? 'Activo' : 'InActivo'
+  end
+
+  #this convert the money value in money format
+  def price_helper(money)
+    number_to_currency(money, unit: "COP ", separator: ",", delimiter: ".")
+  end
 end
